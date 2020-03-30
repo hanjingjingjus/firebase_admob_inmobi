2298d0b8caa9039ad1959188a6f017df2bafe1c3
hanjingjing <jingjing.han@justalk.com>
2020-03-30 11:54:29 +0800

    去除多余引用

--- android/src/main/java/io/flutter/plugins/firebaseadmob/AdRequestBuilderFactory.java
+++ android/src/main/java/io/flutter/plugins/firebaseadmob/AdRequestBuilderFactory.java
@@ -9,7 +9,6 @@ import android.util.Log;
 import com.google.ads.mediation.admob.AdMobAdapter;
 import com.google.ads.mediation.inmobi.InMobiAdapter;
 import com.google.android.gms.ads.AdRequest;
-import com.inmobi.ads.InMobiAdRequest;
 
 import java.util.ArrayList;
 import java.util.Date;
